f4981d0e03e8221d4910a83ee1790f6d948bb26b
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-10-23 15:23:07 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from "next/image"
+
+ 
+export default function Home() {
+  
+  return <div className="p-5">
+          <Image
+            src="/Banner-home-01.jpg"
+            height={0}
+            width={0}
+            className="h-auot w-full"
+            sizes="100vw"
+            alt="Até 55% de desconto esse mês!"
+          />
+  </div>
+}
+
+  
