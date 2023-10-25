ae829493c94e05f0b661f55b1b293c81df2996d3
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-10-25 15:02:30 -0300

    feat: ensure recommended products are showm  on  product details page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,9 @@
 import Image from "next/image"
 import Categories from "./components/categories-overview"
 import { prismaClient } from "@/lib/prisma"
-import ProductList from "./components/product-list"
 import SectionTitle from "./components/section-title"
 import PromoBanner from "./components/promo-banner"
+import ProductList from "@/components/ui/product-list"
 
  
 export default async function Home() {
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
+
 
 interface ProductDetailsPageProps {
     params:{
@@ -14,14 +16,33 @@ const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => 
     const product = await prismaClient.product.findFirst({
         where:{
             slug: slug
+        },
+
+        // aqui quer dizer que ele vai pegar o produto do slug e pegar a category dele a tabela
+        // e vai incluir os produtos dessa category 
+        include:{
+            category:{
+                include :{
+                    products: {
+                        where:{
+                            slug:{
+                                not: slug
+                            }
+                        }
+                    }
+                }
+            }
         }
+
     })
 
     if(!product) return null
 
-    return <div className="flex flex-col gap-8">
+    return <div className="flex flex-col gap-8 pb-8">
         <ProductImages  name={product.name} imageUrls={product.imageUrls}/>
         <ProductInfo product={computProductTotalPrice(product)}/>
+        {/* // aqui eu passei so os produtos da categoria do slug  menos o slug da url*/}
+        <ProductList products={product.category.products}/>
     </div>
 }
  
