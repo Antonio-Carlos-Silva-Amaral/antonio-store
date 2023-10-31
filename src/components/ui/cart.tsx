c79de5438afff1c181456d98449cf38e818059a8
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-10-31 16:54:11 -0300

    feat: ensure cart products can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,26 +5,32 @@ import { CartContext } from "@/provider/cart";
 import CartItem from "./cart-item";
 import { computProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
 
     const {products,subTotal,total,totalDiscount} = useContext(CartContext)
     return ( 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 h-full">
             <Badge className="w-fit gap-1 text-base border-2 uppercase border-primary px-3 py-[0.375rem]" variant="outline">
                 <ShoppingCartIcon size={16}/>
                 Carrinho
             </Badge>
 
 
-            <div className="flex flex-col gap-5">
-            {products.length > 0 ? (
-                products.map(
-                    product => <CartItem key={product.id} product={computProductTotalPrice(product as any) as any} />
-                    )
-            ) : (
-                <p className="text-center font-semibold">Carrinho vázio</p>
-            )}
+            <div className="flex flex-col gap-5 h-full overflow-hidden">
+                <ScrollArea className="h-full">
+                    <div className="flex flex-col h-full gap-4">
+                        {products.length > 0 ? (
+                        products.map(
+                            product => <CartItem key={product.id} product={computProductTotalPrice(product as any) as any} />
+                            )
+                        ) : (
+                            <p className="text-center font-semibold">Carrinho vázio</p>
+                        )}
+                    </div>
+                </ScrollArea>
             </div>
             
             <div className="flex flex-col gap-3">
@@ -55,6 +61,8 @@ const Cart = () => {
                     <p>Total</p>
                     <p>R$ {total.toFixed(2)}</p>
                 </div>
+
+                <Button className="font-bold uppercase mt-7">Finalizar compras</Button>
             </div>
         </div>
      );
