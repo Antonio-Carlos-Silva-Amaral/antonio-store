47190e3816be3cc64071bec51fc8e0bbcb88d501
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-11-05 10:36:37 -0300

    feat: troquei a variante para heading do Badge

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -8,7 +8,7 @@ const CataloPage = async () => {
     const catagories = await prismaClient.category.findMany({})
 
     return <div className="p-5 gap-8 flex flex-col">
-        <Badge className="w-fit gap-1 text-base border-2 uppercase border-primary px-3 py-[0.375rem]" variant="outline">
+        <Badge  variant="heading">
             <ShapesIcon size={16}/>
             Catálogo
         </Badge>
--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -16,7 +16,7 @@ const DealsPage = async () => {
 
     return (
         <div className="p-5 flex flex-col gap-8">
-            <Badge className="w-fit gap-1 text-base border-2 uppercase border-primary px-3 py-[0.375rem]" variant="outline">
+            <Badge variant="heading">
                 <PercentIcon size={16}/>
                 Ofertas
             </Badge>
