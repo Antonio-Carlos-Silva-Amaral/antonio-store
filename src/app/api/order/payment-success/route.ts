855314ecab15ad599e5254c8465fdfa97c6c8ee5
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-11-13 18:07:48 -0300

    tentando novamente

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,39 +1,70 @@
-import { prismaClient } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-import crypto from 'crypto';
+import { prismaClient } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
 
-export const POST = async (request: Request) => {
-  const stripeSignature = request.headers.get('stripe-signature');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY,{
+    apiVersion: "2023-10-16"
+})
 
-  if (!stripeSignature) {
-    return NextResponse.error();
-  }
+export const POST = async (request: Request) =>{
+    // esse ! dis que sempre vai haver uma signature
+    const signature = request.headers.get('stripe-signature')!
+   
 
-  const secret = process.env.STRIPE_WEBHOOK_SECRET_KEY;
+    if(!signature){
+        return NextResponse.error()
+    }
 
-  const payload = await request.text();
+    const text = await request.text()
 
-  const hash = crypto.createHmac('sha256', secret)
-                     .update(payload)
-                     .digest('hex');
+    
+    const event = stripe.webhooks.constructEvent(
+        text,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET_KEY,
+        
+      );
+    
 
-  const isSignatureValid = stripeSignature === `t=${Date.now()},v1=${hash}`;
+        // com esse ckeckout eu consigo tratar qualquer evento tipo quando o pagamento nao for valido 
 
-  if (!isSignatureValid) {
-    return NextResponse.error();
-  }
+        
+    if(event.type === "checkout.session.completed"){
+      const session = event.data.object as any
 
-  // Continue process the webhook payload
-  const event = JSON.parse(payload);
+      console.log({meta: session.metadata})
+        const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+            event.data.object.id,
+            {
+              expand: ['line_items'],
+            }
+          );
+          const lineItems = sessionWithLineItems.line_items;
+          // apartir daqui criar o pedido 
+            console.log("teste:",lineItems)
+          await prismaClient.order.update({
+            where: {
+              id: session.metadata.orderId,
+            },
+            data: {
+              status: "PAYMENT_CONFIRMED",
+            },
+          });
 
-  if (event.type === 'checkout.session.completed') {
-    // Handle your logic for checkout session completed
-    const session = event.data.object as any;
+        
+    }
+    
+  
 
-    console.log({ meta: session.metadata });
+    return new Response(JSON.stringify({ received: true }), {
+      status: 200, // Código de status HTTP
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-    // ... rest of your logic
-  }
+    // agente usa o stripe secret key, usa o signature, e o stripe webhook secret key para verificar
+    // se o evento realmente existe 
 
-  return NextResponse.json({ received: true });
-};
+
+}
