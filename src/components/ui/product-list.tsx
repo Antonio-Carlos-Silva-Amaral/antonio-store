cc1bebbf293733fcf17174d97a6380afc48f10b6
Antonio-Carlos-Silva-Amaral <carlosaantonio998@gmail.com>
2023-11-14 09:17:21 -0300

    feat: add key

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -10,7 +10,7 @@ const ProductList = ({products}: ProductListProps) => {
     return ( 
         <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
             {products.map((product) => (
-                    <ProductItem  product={computProductTotalPrice(product)}
+                    <ProductItem key={product.id}  product={computProductTotalPrice(product)}
                     className="w-[156px] lg:w-[200px] lg:min-w-[200px]"
                      />
             ))}
